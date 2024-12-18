9463a7204f2d80ae144a2b730a67db71747dc7f1
pablosantiago-ar <pablo.santiago.ar@gmail.com>
2024-12-18 07:50:41 -0300

    Update Enviar_Email_Outlook.py

--- Enviar_Email_Outlook.py
+++ Enviar_Email_Outlook.py
@@ -8,13 +8,17 @@ outlook = win32.Dispatch('outlook.application')
 
 #Cria um novo email
 email = outlook.CreateItem(0)
+
 #Entre as aspas, colocar o email dos destinatários, separando-os por ';'. Exemplo: "example@gmail.com;example2@gmail.com"
 #Ao copiar dados de email do Excel, os mesmos viram com quebra de linha, o qual não funcionará no código.
 #Para retirar a quebra de linha e adicionar o caractere separador ';', ir ao site https://www.4devs.com.br/remover_trocar_quebra_linha
 #Após isso, colar os emails no espaço destinado, e escolher para separar por caractere ;. copie o resultado e cole dentro das aspas do email.to
+#O limite de destinatários por mensagem é de 500 usuários!
 email.to ="destinatário"
+
 #Assunto do email
 email.Subject = "teste automação"
+
 #Texto de dentro do email, escrever a mensagem com base em HTML.
 email.HTMLBody = """
 <h1>O furo esmagador</h1>
@@ -44,6 +48,7 @@ email.HTMLBody = """
 #Declarar a uma variável anexo, atribuindo a ela o destino do arquivo que deseja anexar
 anexo = "C://Users/Pablo Rodrigues/Documents/enviaremail.py"
 email.Attachments.Add(anexo)
+
 #Envia o email
 email.Send()
 print("Email enviado")
